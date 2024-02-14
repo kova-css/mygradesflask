df3a0fa5ccb2e56e19c5fd454c1a2fb8dc7b0945
Krisztián <76844569+kova-css@users.noreply.github.com>
2024-02-14 21:55:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,40 @@
-from flask import Flask, request
-import requests
+from flask import Flask, request, Response
 from bs4 import BeautifulSoup
+import requests
+import json
+
+class CustomJSONEncoder(json.JSONEncoder):
+    def __init__(self, *args, **kwargs):
+        kwargs['ensure_ascii'] = False
+        super(CustomJSONEncoder, self).__init__(*args, **kwargs)
 
 app = Flask(__name__)
+app.json_encoder = CustomJSONEncoder
 
 @app.route('/', methods=['POST'])
 def fetch_data():
     req_data = request.get_json()
     url = req_data.get('url')
     data = req_data.get('data')
+    method = req_data.get('method')
 
     response = requests.post(url, data=data, allow_redirects=False)
     response.encoding = 'utf-8'
 
-    if response.status_code == 200:
-        return 'fail'
-    elif response.status_code == 302:
-        response = requests.post(url, data=data)
-        response.encoding = 'utf-8'
-        soup = BeautifulSoup(response.text, 'html.parser')
-        materials = soup.find_all('th', {'class': 'rotate'})
-        data = []
-        for material in materials[1:]:
-            name = material.get('name')
-            data.append({'material': material.text.strip(), 'id': name[1:]})
-    return data
+    if method == 'materials':
+        if response.status_code == 200:
+            return 'fail'
+        elif response.status_code == 302:
+            response = requests.post(url, data=data)
+            response.encoding = 'utf-8'
+            soup = BeautifulSoup(response.text, 'html.parser')
+            materials = soup.find_all('th', {'class': 'rotate'})
+            data = []
+            for material in materials[1:]:
+                name = material.get('name')
+                data.append({'material': material.text.strip(), 'id': name[1:]})
+        return Response(json.dumps(data, ensure_ascii=False), mimetype='application/json')
+    else:
+        return 'Invalid method'
 
 app.run()
