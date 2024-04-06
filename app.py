ed7bd8df44b5d8675f061bf7f75da2a934432fd6
Krisztián <76844569+kova-css@users.noreply.github.com>
2024-04-06 19:45:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ app.json_encoder = CustomJSONEncoder
 
 @app.route('/', methods=['POST'])
 def fetch_data():
-    return '{"name": "KOVÁCS KRISZTIÁN-HUNOR", "premium": false, "materials": [{"material": "Test", "id": "1"}], "absences": [[["31.01", true]]], "grades": [[["8", "11.10"], ["6", "22.11"], ["7", "07.12"], ["9", "27.03"], ["10", "05.04"]]]}'
+    return '{"name": "KOVÁCS KRISZTIÁN-HUNOR", "premium": false, "materials": [{"material": "Test1", "id": "1"}, {"material": "Test2", "id": "2"}], "absences": [[["31.01", true]], [["31.01", false]]], "grades": [[["8", "11.10"], ["6", "22.11"], ["7", "07.12"], ["9", "27.03"], ["10", "05.04"]], [["8", "11.10"]]]}'
 
 app.run()
